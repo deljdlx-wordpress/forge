93dc70e2b88035b23bb6b8c37fd893888e5e38f3
Julien Delsescaux <jdlxt@locahost.dev>
2023-02-05 11:10:12 +0100

    sync

--- public/content/themes/forge/templates/partials/navbar-right.blade.php
+++ public/content/themes/forge/templates/partials/navbar-right.blade.php
@@ -2,6 +2,6 @@
         <img src="@themeUri('assets/images/bench-01.png')" style=""/>
     </div>
     <div class="sidebar-right__content" style="">
-        <h3>Right column</h3>
+        {{-- <h3>Right column</h3> --}}
         <?php wp_forge()->sidebar->render('sidebar-right'); ?>
     </div>
